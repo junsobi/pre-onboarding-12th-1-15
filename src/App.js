c0338ebb0b1ddc3b91fdbdba58a83dd7dc03ca16
seongmin <seongm22n@gmail.com>
2023-08-23 18:00:27 +0900

    init : 프로젝트 초기 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 import './App.css';
-// import Header from './components/Header';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Outlet />
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react';
+
+function Header() {
+  return (
+    <>
+      헤더~~
+      <br />
+    </>
+  );
+}
+
+export default Header;
