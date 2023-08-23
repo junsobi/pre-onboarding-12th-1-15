e5dff8653558668ef8c10185d35aab9449167067
seongmin <seongm22n@gmail.com>
2023-08-24 00:13:02 +0900

    style : tailwindcss 기본 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      colors: {},
+      boxShadow: {
+        custom:
+          '0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 0 -3px #f6f6f6, 0 9px 1px -3px rgba(0, 0, 0, 0.2), 0 16px 0 -6px #f6f6f6, 0 17px 2px -6px rgba(0, 0, 0, 0.2)',
+      },
+      margin: {},
+    },
+    width: {
+      headerWCustom: 'rem',
+    },
   },
   plugins: [],
-}
-
+};
