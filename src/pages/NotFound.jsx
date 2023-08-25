43516461de15e7326672a09c7a55905c776b8455
JungHyun <wjstkwhwjdgus@gmail.com>
2023-08-25 19:54:17 +0900

    feat : Not Found 페이지 작성

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from '../components/Layout/Layout';
+import { ReactComponent as NotFoundIcon } from '../NotFoundIcon.svg';
 
 function NotFound() {
-  return <>에러</>;
+  return (
+    <Layout title="404 Not Found">
+      <NotFoundIcon width="350px" height="400px" />
+      <h1 className="font-bold text-2xl mb-10">페이지를 찾을 수 없습니다.</h1>
+    </Layout>
+  );
 }
 
 export default NotFound;
